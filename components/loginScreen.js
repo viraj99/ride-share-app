ead3b69edba50993ffec0b9d89636be28b7e9e96
lauradurandv <lauradurandv@gmail.com>
2019-01-29 12:39:44 -0500

    username and password inputs

--- components/loginScreen.js
+++ components/loginScreen.js
@@ -1,14 +1,48 @@
 import React, { Component } from 'react';
-import { Platform, StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, TextInput } from 'react-native';
 
 
 export default class Login extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      username:'',
+      password:''
+    };
+
+    this.handleUsername = this.handleUsername.bind(this);
+    this.handlePassword = this.handlePassword.bind(this);
+  }
+
+  handleUsername(event) {
+    this.setState({
+      username: event.target.value
+    });
+  }
+
+  handlePassword(event) {
+    this.setState({
+      password: event.target.value
+    });
+  }
+
   render() {
     return (
       <View style={styles.container}>
         <Text>
           login screen
         </Text>
+        <TextInput
+          style={styles.textInput}
+          placeholder="Username"
+          name="username"
+          onChangeText={this.handleUsername}
+        />
+        <TextInput
+          style={styles.textInput}
+          placeholder="Password"
+          onChangeText={this.handlePassword}
+        />
       </View>
     );
   }
@@ -20,5 +54,9 @@ const styles = StyleSheet.create({
     justifyContent: 'center',
     alignItems: 'center',
     backgroundColor: '#F5FCFF',
-  }
+  },
+  textInput: {
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
 });
