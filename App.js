e7c09c296d2380755b279992e5777e28865bf1bd
lauradurandv <lauradurandv@gmail.com>
2019-02-01 10:47:36 -0500

    change in App.js

--- App.js
+++ App.js
@@ -1,15 +1,9 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- *
- * @format
- * @flow
- */
 
 import React, { Component } from 'react';
 import {
   Platform, StyleSheet, Text, View,
 } from 'react-native';
+import App from './app/index';
 
 const instructions = Platform.select({
   ios: 'Press Cmd+R to reload,\n' + 'Cmd+D or shake for dev menu',
