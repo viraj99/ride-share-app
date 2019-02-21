de1ce054218325a906fcda39dcd74278111e8781
AndreaHG <andrea.hrnndzg@gmail.com>
2019-02-21 14:23:19 -0500

    add dummy views to test navigation

---
+++ app/views/RideView.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import {
+  Text, View, ScrollView, StatusBar, TouchableOpacity,
+} from 'react-native';
+
+const RideView = () => (
+  <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
+    <Text>RIDE VIEW</Text>
+  </View>
+);
+
+export default RideView;
