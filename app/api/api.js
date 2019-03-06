3274080193c6db214a633345f580de9b46ab6033
AndreaHG <andrea.hrnndzg@gmail.com>
2019-03-06 14:59:51 -0500

    set up main view mock api endpoints

--- app/api/api.js
+++ app/api/api.js
@@ -1,7 +1,13 @@
-import { RIDES, HEADERS } from '../utils/urls';
+import { RIDES, SCHEDULEDRIDES, APPROVEDRIDES } from '../utils/urls';
 
 export default {
   getRides() {
     return fetch(RIDES).then(response => response.json());
+  },
+  getScheduledRides() {
+    return fetch(SCHEDULEDRIDES).then(response => response.json());
+  },
+  getApprovedRides() {
+    return fetch(APPROVEDRIDES).then(response => response.json());
   },
 };
--- app/utils/urls.js
+++ app/utils/urls.js
@@ -1,5 +1,7 @@
 export const LOGIN = '';
 export const RIDES = 'https://8a8fd4a9-859b-42f0-a8cc-1ef4da828d9a.mock.pstmn.io/v1/rides';
+export const SCHEDULEDRIDES = 'https://8a8fd4a9-859b-42f0-a8cc-1ef4da828d9a.mock.pstmn.io/v1/rides?state=scheduled';
+export const APPROVEDRIDES = 'https://8a8fd4a9-859b-42f0-a8cc-1ef4da828d9a.mock.pstmn.io/v1/rides?state=approved';
 
 export const HEADERS = {
   Accept: 'application/json',
