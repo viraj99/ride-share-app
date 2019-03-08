c050109cbfff21c5b08727fb31438909cab970cd
AndreaHG <andrea.hrnndzg@gmail.com>
2019-03-08 18:55:40 -0500

    add seperate folder for ride view

--- app/views/index.js
+++ app/views/index.js
@@ -2,7 +2,7 @@ import Login from './Login/loginScreen';
 import MainView from './MainView/MainView';
 import AgendaView from './AgendaView/AgendaView';
 import AuthLoadingScreen from './AuthLoadingScreen';
-import RideView from './RideView';
+import RideView from './RideView/RideView';
 import DriverScheduleView from './DriverScheduleView';
 import RidesRequestedView from './RidesRequestedView';
 import RequestedRidesDetails from './RequestedRidesDetails';
