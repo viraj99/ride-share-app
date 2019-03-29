052e95e68f31982a2ca21d02f45fe0914bc13827
AndreaHG <andrea.hrnndzg@gmail.com>
2019-03-29 16:49:28 -0400

    add crossplatform touchable press

---
+++ app/utils/Touch/index.android.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import { TouchableNativeFeedback, View } from 'react-native';
+
+const Touch = ({
+  children, style, onPress, ...props
+}) => (
+  <TouchableNativeFeedback onPress={onPress} {...props}>
+    <View style={style}>{children}</View>
+  </TouchableNativeFeedback>
+);
+
+export default Touch;
