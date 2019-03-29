6fb2ea563ccd97f9ba58eddca17d97e3058c4921
AndreaHG <andrea.hrnndzg@gmail.com>
2019-03-29 17:07:40 -0400

    set up rides lists views with data

--- app/views/index.js
+++ app/views/index.js
@@ -3,8 +3,8 @@ import MainView from './MainView/MainView';
 import AgendaView from './AgendaView/AgendaView';
 import AuthLoadingScreen from './AuthLoadingScreen';
 import RideView from './RideView';
-import DriverScheduleView from './DriverScheduleView';
-import RidesRequestedView from './RidesRequestedView';
+import DriverScheduleView from './DriverScheduleView/DriverScheduleView';
+import RidesRequestedView from './RidesRequestedView/RidesRequestedView';
 import RequestedRidesDetails from './RequestedRidesDetails';
 import Settings from './settings/settingsScreen';
 
