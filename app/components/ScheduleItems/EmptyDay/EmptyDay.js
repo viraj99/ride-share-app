e3b85680dbb6a8e333a9ff3de13de1a40069506b
Daniel Blat <blatde@Daniels-MacBook-Pro-3.local>
2019-04-10 15:57:41 -0400

    quick fix

--- app/components/ScheduleItems/EmptyDay/EmptyDay.js
+++ app/components/ScheduleItems/EmptyDay/EmptyDay.js
@@ -2,16 +2,15 @@ import React from 'react';
 import {
   Text, View,
 } from 'react-native';
-import moment from 'moment';
+import DateDisplay from '../DateDisplay/DateDisplay';
 import styles from './styles';
 
 const EmptyDay = ({ date }) => (
 
   <View style={styles.container}>
-    <View style={styles.dateContainer}>
-      <Text style={styles.dateAbbrev}>{moment(date, 'X').format('ddd')}</Text>
-      <Text style={styles.dateNumber}>{moment(date, 'X').format('D')}</Text>
-    </View>
+    <DateDisplay
+      date={date}
+    />
     <View style={styles.contentContainer}>
       <Text style={styles.emptyText}>Nothing scheduled yet. Tap to add availability.</Text>
     </View>
