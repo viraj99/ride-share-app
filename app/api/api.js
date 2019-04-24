9bcc4fa95c642fb70884eef84dd2b1accba1fb38
AndreaHG <andrea.hrnndzg@gmail.com>
2019-04-24 13:42:18 -0400

    update fetch requests methods

--- app/api/api.js
+++ app/api/api.js
@@ -1,8 +1,23 @@
-import { RIDES, SCHEDULEDRIDES, APPROVEDRIDES } from '../utils/urls';
+import {
+  LOGIN, RIDES, SCHEDULEDRIDES, APPROVEDRIDES, driverRides,
+} from '../utils/urls';
+import apiWrapper from './apiWrapper';
 
 export default {
-  getRides() {
-    return fetch(RIDES).then(response => response.json());
+  // ex. of how the post and etc. requests can be written
+  login(email, password) {
+    return apiWrapper({
+      path: LOGIN,
+      body: { email, password },
+      method: 'POST',
+    }).then(res => res.json());
+  },
+  getRides(token) {
+    return apiWrapper({
+      path: RIDES,
+      params: driverRides,
+      headers: { token },
+    }).then(res => res.json());
   },
   getScheduledRides() {
     return fetch(SCHEDULEDRIDES).then(response => response.json());
