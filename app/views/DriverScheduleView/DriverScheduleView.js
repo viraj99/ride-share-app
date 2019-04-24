b2cd913478ab434c5cd7cd9caace6882d0f3c001
AndreaHG <andrea.hrnndzg@gmail.com>
2019-04-24 13:31:14 -0400

    update vars to match api response

--- app/views/DriverScheduleView/DriverScheduleView.js
+++ app/views/DriverScheduleView/DriverScheduleView.js
@@ -4,9 +4,9 @@ import { RideListItem } from '../../components/RideListItem';
 
 const DriverScheduleView = (props) => {
   const { navigation } = props;
-  const scheduledRides = navigation.getParam('CustomRidesData');
+  const scheduledRides = navigation.getParam('scheduledRides');
 
-  keyExtractor = item => item.id.toString();
+  keyExtractor = item => item.driver_id.toString();
 
   navigateToRideView = () => {
     navigation.navigate('RideView');
@@ -14,9 +14,9 @@ const DriverScheduleView = (props) => {
 
   renderRideList = ({ item, index }) => {
     const name = item.riderName;
-    const date = item.pickupTime;
-    const time = item.pickupTime;
-    const address = `At ${item.pickupLocation.street_address}`;
+    const date = item.pick_up_time;
+    const time = item.pick_up_time;
+    const address = `At ${item.start_location.street}`;
 
     const underlayColor = '#FFF';
     return (
