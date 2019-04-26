bf6b6c9b5981312734f38feef69e423d68e21e8f
AndreaHG <andrea.hrnndzg@gmail.com>
2019-04-26 11:15:31 -0400

    update dependencies

--- android/app/src/main/java/com/rideshareapp/MainApplication.java
+++ android/app/src/main/java/com/rideshareapp/MainApplication.java
@@ -3,6 +3,8 @@ package com.rideshareapp;
 import android.app.Application;
 
 import com.facebook.react.ReactApplication;
+import com.reactnativecommunity.slider.ReactSliderPackage;
+import com.reactnativecommunity.viewpager.RNCViewPagerPackage;
 import com.reactnativecommunity.asyncstorage.AsyncStoragePackage;
 import com.swmansion.gesturehandler.react.RNGestureHandlerPackage;
 import com.oblador.vectoricons.VectorIconsPackage;
@@ -26,6 +28,8 @@ public class MainApplication extends Application implements ReactApplication {
     protected List<ReactPackage> getPackages() {
       return Arrays.<ReactPackage>asList(
           new MainReactPackage(),
+            new ReactSliderPackage(),
+            new RNCViewPagerPackage(),
             new AsyncStoragePackage(),
             new RNGestureHandlerPackage(),
             new VectorIconsPackage()
