593b755d62bd09f32f59889a1272a0ee63edfcf5
AndreaHG <andrea.hrnndzg@gmail.com>
2019-05-02 12:36:42 -0400

    update main view header

--- app/components/Header/Header.js
+++ app/components/Header/Header.js
@@ -1,21 +1,29 @@
 import React from 'react';
-import { Text, View, TouchableOpacity } from 'react-native';
+import {
+  Text, View, TouchableOpacity, Platform,
+} from 'react-native';
 import Icon from 'react-native-vector-icons/MaterialCommunityIcons';
+import LinearGradient from 'react-native-linear-gradient';
 
 // import PropTypes from 'prop-types';
 import styles from './styles';
 
 const Header = ({ onPress }) => (
-  <View style={styles.headerContainer}>
+  <LinearGradient
+    start={{ x: 0, y: 0 }}
+    end={{ x: 1, y: 0 }}
+    colors={Platform.OS === 'ios' ? ['#45bb92', '#15a76a', '#11a461'] : ['#1EAA70', '#1EAA70']}
+    style={styles.headerContainer}
+  >
     <View style={styles.leftContainer}>
-      <Text style={styles.titleText}>Title or name</Text>
+      <Text style={styles.titleText}>Welcome</Text>
     </View>
     <View style={styles.rightContainer}>
       <TouchableOpacity onPress={onPress}>
         <Icon color="white" name="settings" size={30} />
       </TouchableOpacity>
     </View>
-  </View>
+  </LinearGradient>
 );
 
 // Header.propTypes = {
--- app/components/Header/styles.js
+++ app/components/Header/styles.js
@@ -3,8 +3,6 @@ import { getStatusBarHeight } from './StatusBar';
 
 export default StyleSheet.create({
   headerContainer: {
-    backgroundColor: '#1EAA70',
-    borderBottomColor: '#f2f2f2',
     paddingHorizontal: 10,
     paddingTop: Platform.OS === 'ios' ? getStatusBarHeight() : 0,
     flexDirection: 'row',
@@ -18,7 +16,6 @@ export default StyleSheet.create({
     },
     shadowOpacity: 0.25,
     shadowRadius: 3.84,
-
     elevation: 5,
   },
   centerContainer: {
@@ -32,13 +29,6 @@ export default StyleSheet.create({
   leftContainer: {
     flex: 1,
     alignItems: 'flex-start',
-  },
-  userPic: {
-    width: 40,
-    height: 40,
-    borderRadius: 40 / 2,
-    borderWidth: 1.5,
-    borderColor: '#fff',
   },
   titleText: {
     color: 'white',
