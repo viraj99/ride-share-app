31a338e80212a4b5fd1263c7f8ff8c61d65478a9
andreaHG <andrea.hrnndzg@gmail.com>
2019-11-12 09:55:49 -0500

    add current api files

---
+++ app/api/apiWrapper.js
@@ -0,0 +1,49 @@
+import {apiBaseUrl} from '../utils/urls';
+
+const buildHeaders = (additionalHeaders, token) => {
+  const authHeaders = token
+    ? {
+        token: `${token}`,
+      }
+    : {};
+  return {
+    Accept: 'application/json',
+    'Content-Type': 'application/json',
+    ...authHeaders,
+    ...additionalHeaders,
+  };
+};
+
+const buildUrl = (path, params) => {
+  let url = `${apiBaseUrl}${path}`;
+  url = params ? `${apiBaseUrl}${path}${params}` : url;
+  return url;
+};
+
+const apiWrapper = async request => {
+  const {
+    path,
+    params,
+    body,
+    method = 'GET',
+    headers: suppliedHeaders = {},
+    token = null,
+  } = request;
+
+  if (!path) {
+    throw new Error("'path' property not specified for fetch call");
+  }
+  const url = buildUrl(path, params);
+  const fetchConfig = {
+    method,
+    headers: buildHeaders(suppliedHeaders, token),
+    body: JSON.stringify(body),
+  };
+  const response = await fetch(url, fetchConfig);
+  if (response.ok) {
+    return response;
+  }
+  throw response;
+};
+
+export default apiWrapper;
