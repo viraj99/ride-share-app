73892582ec4279b86e900366ea800ef12f17ce42
mamooredesigns <mamooredesigns@gmail.com>
2020-01-03 15:48:37 -0500

    WIP - recurring end date displaying, but not accurate

--- app/views/AgendaView/AgendaView.js
+++ app/views/AgendaView/AgendaView.js
@@ -13,12 +13,10 @@ class AgendaView extends React.Component {
   constructor(props){
     super(props);
     this.state={
-
     }
   }
 
   componentDidMount = async () => {
-    console.log("*****PAGE MOUNTING*****")
     const value = await AsyncStorage.getItem('token');
     const parsedValue = JSON.parse(value);
     const realToken = parsedValue.token;
@@ -28,13 +26,13 @@ class AgendaView extends React.Component {
     this.getAvailability();
   }
 
-  getAvailability = async() => {
-    let token = await AsyncStorage.getItem('token')
-    token = JSON.parse(token)
-    let avails = await api.getAvailabilities(token.token)
-    console.log("response maybe? ", avails.json)
+  getAvailability = async () => {
+    let token = this.state.token
+    let avails = await api.getAvailabilities(token)
+    console.log("from API: ", avails)
     
     const result = []
+    const others = []
     const map = new Map()
     for (const item of avails.json) {
       if (!map.has(item.eventId)){
@@ -46,90 +44,103 @@ class AgendaView extends React.Component {
           isRecurring: item.isRecurring,
           day: moment(item.startTime).format("dddd")
         })
-      }
-      console.log("testing this: ", result)
+      } else {
+        map.set(item.eventId, true);
+        others.push({
+          id: item.eventId,
+          startTime: item.startTime,
+          endTime: item.endTime,
+          isRecurring: item.isRecurring,
+          day: moment(item.startTime).format("dddd")
+        })
+      }  
       this.setState({
-        response: result
+        response: result,
+        others: others,
       })
     }
   }
 
-  deleteAvail = (eventId) => {
-    let token = this.state.token
-    console.log("in deleteAvail fx: ", token)
-    console.log("in deleteAvail fx: ", eventId)
-    api.deleteAvailability(token, eventId)
+  testAMatch = (item) => {
+    if (item.isRecurring === true) {
+      console.log("starting test a match")
+      let { response, others } = this.state
+      console.log("in test a match fx: ", response)
+      console.log("in test a match fx: ", others)
+      let updatedRecur
+      
+      for (const each of response) {
+        const arrayOfMatchingDates = []
+        const lastDate = []
+        if (each.isRecurring === true) {
+          console.log("testing a match")
+          const testItem = each.id
+          console.log("match is for item #: ", testItem)
+          others.map(eachItem => {
+            console.log("eachItem I'm testing is: ", eachItem)
+            if (eachItem.id === testItem) {
+              // arrayOfMatchingDates.push(eachItem.startTime)
+              // console.log("new array of matching:", arrayOfMatchingDates)
+              // const last = arrayOfMatchingDates.length
+              // console.log("the array of matches length is: ", last)
+              // console.log("the last of matching dates is: ", arrayOfMatchingDates[last-1])
+              // let lastDay = arrayOfMatchingDates[last-1]
+              lastDate.push(eachItem.startTime)
+            }
+            // return endOfRecurrence
+          })
+          updatedRecur = lastDate[lastDate.length-1]
+          console.log("Good! ", lastDate[lastDate.length-1])
+          return updatedRecur
+        }
+      }
+    }
   }
 
   renderItem = (item) => {
-    let token = this.state.token
-    console.log("token is: ", token)
-
-    console.log("each item: ", item)
     let id = item.id
     let date = moment(item.startTime).format("MMMM D, YYYY")
     let start = moment.utc(item.startTime).format("h:mm A")
     let end = moment.utc(item.endTime).format("h:mm A")
     let day = moment(item.startTime).format("dddd")+"s"
-    let endDate = moment(item.endTime).format("MMMM D, YYYY")
-    let driverToken = this.state.token
-    console.log("in return, id is: ", id)
-    console.log("in return, token is: ", driverToken)
+    let endDate = this.testAMatch(item);
+    console.log("what happens now? ", endDate)
+    let ending = moment(endDate).format("MMMM D, YYYY")
+    console.log("maybe? ", ending)
     
-      return(
-        <View
-          style={[styles.availListItem]}>
-          <View style={styles.leftList}>
-            <TouchableOpacity onPress={() => this.redirectToAddAvail(item)}>
-              <Icon color={'#ff8262'} name="pencil" size={30} />
-            </TouchableOpacity>
-          </View>
-
-          <View style={styles.centerList}>
-            {item.isRecurring && <Text style={styles.flatListText}>{day}</Text>}
-            {!item.isRecurring && <Text style={styles.flatListText}>{date}</Text> }
-            <Text style={styles.flatListText}>{start} to {end}</Text>
-            {/* <Text>until {endDate}</Text> */}
-          </View>
-          
-          <View style={styles.rightList}>
-            <TouchableOpacity onPress={() => this.deleteAvail(id)}>
-              <Icon color={'#ff8262'} name="delete" size={30} />
-            </TouchableOpacity>
-          </View>
+    return(
+      <View
+        style={[styles.availListItem]}>
+        <View style={styles.leftList}>
+          <TouchableOpacity onPress={() => this.redirectToAddAvail(item)}>
+            <Icon color={'#ff8262'} name="pencil" size={30} />
+          </TouchableOpacity>
         </View>
-      )
-    // } else {
-      
-    //   return(
-    //     <View
-    //       style={[styles.availListItem]}>
-    //         <View style={styles.leftList}>
-    //           <TouchableOpacity onPress={() => this.redirectToAddAvail(item)}>
-    //             <Icon color={'#ff8262'} name="pencil" size={30} />
-    //           </TouchableOpacity>
-    //         </View>
 
-    //         <View style={styles.centerList}>
-              
-    //           <Text style={styles.flatListText}>{start} to {end}</Text>
-    //         </View>
-    //         <View style={styles.rightContainer}>
-            
-    //         <View style={styles.rightList}>
-    //           <TouchableOpacity onPress={() => this.deleteAvail(id)}>
-    //             <Icon color={'#ff8262'} name="delete" size={30} />
-    //           </TouchableOpacity>
-    //         </View>
-    //       </View>
-    //     </View>
-    //   )
-    // }
+        <View style={styles.centerList}>
+          {item.isRecurring && <Text style={styles.flatListText}>{day}</Text>}
+          {!item.isRecurring && <Text style={styles.flatListText}>{date}</Text> }
+          <Text style={styles.flatListText}>{start} to {end}</Text>
+          {item.isRecurring && <Text style={styles.flatListText}>until {ending}</Text>}
+        </View>
+        
+        <View style={styles.rightList}>
+          <TouchableOpacity onPress={() => this.deleteAvail(id)}>
+            <Icon color={'#ff8262'} name="delete" size={30} />
+          </TouchableOpacity>
+        </View>
+      </View>
+    )
   }
 
   redirectToAddAvail = (item) => {
     const { navigation } = this.props;
     navigation.navigate('AvailabilityView', {item})
+  }
+
+  deleteAvail = (eventId) => {
+    let token = this.state.token
+    api.deleteAvailability(token, eventId)
   }
 
   backButton = () => {
@@ -138,16 +149,12 @@ class AgendaView extends React.Component {
   }
 
   render(){
-    let noItem = {
-      id: null,
-    };
+    let noItem = { id: null }
+    console.log("items from API call: ", this.state.response)
+    console.log("recurrences in API call: ", this.state.others)
     return(
       <Container>
         <NavigationEvents onDidFocus={() => this.getAvailability()} />
-        {/* <RegisterHeader
-          onPress={() => navigation.navigate('MainView')}
-          iconColor="#475c67"
-        /> */}
 
         <View
           style={[styles.headerContainer]}>
@@ -161,7 +168,6 @@ class AgendaView extends React.Component {
           </View>
         </View>
 
-
         <ScrollView>
           {this.state.response && 
             <FlatList
@@ -170,6 +176,7 @@ class AgendaView extends React.Component {
               keyExtractor={item => item.eventId}
             />
           }
+
           <View style={styles.footer}>
             <TouchableOpacity style={styles.buttonContainer} onPress={() => this.redirectToAddAvail(noItem)}>
               <View style={styles.buttonWrapper}>
