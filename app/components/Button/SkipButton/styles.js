cd395371c2f647c568ae3461da8946f0c7cd3759
osbaldo aguilar <osbaldoaguilar@osbaldos-MacBook-Air.local>
2020-01-29 14:50:27 -0500

    Less Popups and SkipButton

---
+++ app/components/Button/SkipButton/styles.js
@@ -0,0 +1,55 @@
+// Start Button styles
+import { Platform, StyleSheet, Dimensions } from 'react-native';
+
+const { height, width } = Dimensions.get('window');
+// #ff8262 start base
+// #ff6272 final choice will be this pink use for resume change of state
+
+export default StyleSheet.create({
+  linearGradient: {
+    borderRadius: 30,
+    marginHorizontal: 10,
+    height: 50,
+    width: width / 3,
+    alignSelf: 'center',
+    justifyContent: 'center'
+  },
+  circleButtonContainer: {
+    backgroundColor: '#fff',
+    borderWidth: 2,
+    borderColor: '#fff',
+    borderRadius: 20,
+    width: 35,
+    height: 35,
+    alignItems: 'flex-end',
+    justifyContent: 'center',
+    shadowColor: '#000',
+    shadowOffset: {
+      width: 0,
+      height: 3
+    },
+    shadowOpacity: 0.27,
+    shadowRadius: 4.65,
+    elevation: 6,
+    paddingBottom: 10
+  },
+  buttonWrapper: {
+    paddingHorizontal: 10,
+    flexDirection: 'row',
+    alignContent: 'center'
+    // paddingBottom: 10
+  },
+  buttonText: {
+    paddingLeft: 18,
+    color: '#fff',
+    fontWeight: '600',
+    fontSize: 22
+    // paddingBottom: 10
+  },
+  startTextContainer: {
+    flex: 2,
+    alignItems: 'flex-start',
+    justifyContent: 'center'
+    // paddingBottom: 10
+  }
+});
