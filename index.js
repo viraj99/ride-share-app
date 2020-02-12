93789f7510c08fbcbe87bd7c94d0099b5d99b0c6
mamooredesigns <mamooredesigns@gmail.com>
2020-02-12 14:12:15 -0500

    removed the disableYellowBox notation in index.js

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ import { AppRegistry } from 'react-native';
 import App from './App';
 import { name as appName } from './app.json';
 
-console.disableYellowBox = true;
+// console.disableYellowBox = true;
 
 // XMLHttpRequest = GLOBAL.originalXMLHttpRequest ?
 //     GLOBAL.originalXMLHttpRequest :
