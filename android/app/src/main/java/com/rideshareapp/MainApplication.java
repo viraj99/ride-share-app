1e72173853a20ea9189dd0cb4280d3800b4983b5
mamooredesigns <mamooredesigns@gmail.com>
2020-04-10 18:08:38 -0400

    fixed, with location added to login screen while testing

--- android/app/src/main/java/com/rideshareapp/MainApplication.java
+++ android/app/src/main/java/com/rideshareapp/MainApplication.java
@@ -9,6 +9,7 @@ import com.facebook.react.ReactPackage;
 import com.facebook.soloader.SoLoader;
 import java.lang.reflect.InvocationTargetException;
 import java.util.List;
+import com.devfd.RNGeocoder.RNGeocoderPackage;
 
 public class MainApplication extends Application implements ReactApplication {
 
@@ -44,6 +45,13 @@ public class MainApplication extends Application implements ReactApplication {
     super.onCreate();
     SoLoader.init(this, /* native exopackage */ false);
     initializeFlipper(this); // Remove this line if you don't want Flipper enabled
+  }
+
+  @Override
+  protected List<ReactPackage> getPackages() {
+    return Arrays.<ReactPackage>asList(
+            new MainReactPackage(),
+            new RNGeocoderPackage()); // <------ add this
   }
 
   /**
--- app/views/Welcome/Welcome.js
+++ app/views/Welcome/Welcome.js
@@ -1,12 +1,12 @@
 import React from 'react';
-import {Text, View, Image, StatusBar} from 'react-native';
+import { Text, View, Image, StatusBar } from 'react-native';
 
 import styles from './styles';
 import logo from '../../utils/images/route.png';
 
-import {CalendarButton} from '../../components/Button';
+import { CalendarButton } from '../../components/Button';
 
-const Welcome = ({navigation}) => (
+const Welcome = ({ navigation }) => (
   <View style={styles.container}>
     <StatusBar backgroundColor="#fff" barStyle="dark-content" />
     <View style={styles.header}>
@@ -18,7 +18,7 @@ const Welcome = ({navigation}) => (
         {/* Icon made by Map & Navigation from www.flaticon.com */}
       </View>
     </View>
-    <View style={{paddingHorizontal: 32, paddingTop: 16}}>
+    <View style={{ paddingHorizontal: 32, paddingTop: 16 }}>
       <CalendarButton
         title="Login"
         onPress={() => navigation.navigate('Login')}
